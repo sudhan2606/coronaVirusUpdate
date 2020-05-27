93870107d376455379f07ab41649c0f8635df47f
sudhan2606 <sudhanshumishra26sm@gmail.com>
2020-05-27 15:56:40 +0530

    Updated on 27-05-2020

---
+++ src/main/java/com/tracker_sample/corona_virus_tracker/services/CoronaVirusDataService.java
@@ -0,0 +1,59 @@
+package com.tracker_sample.corona_virus_tracker.services;
+
+import com.tracker_sample.corona_virus_tracker.models.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.awt.*;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CoronaVirusDataService {
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private java.util.List<LocationStats> allStats = new ArrayList<>();
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+    @PostConstruct
+    @Scheduled(cron = "* * 1 * * *") //* from L to R sec min hour day month year
+    public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        //System.out.println(httpResponse.body());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+
+        for (CSVRecord record : records) {
+            LocationStats locationStat = new LocationStats();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+
+            int latestCases = Integer.parseInt(record.get(record.size()-1));
+            int prevDayCases = Integer.parseInt(record.get(record.size()-2));
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromPrevDay(latestCases-prevDayCases);
+
+            //System.out.println(locationStat);
+            newStats.add(locationStat);
+            //String state = record.get("Province/State");
+            //System.out.println(state);
+            //String customerNo = record.get("CustomerNo");
+            //String name = record.get("Name");
+        }
+        this.allStats = newStats;
+    }
+}
---
+++ src/test/java/com/tracker_sample/corona_virus_tracker/CoronaVirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker_sample.corona_virus_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaVirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
